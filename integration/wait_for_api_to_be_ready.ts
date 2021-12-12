5ff4c52519cd950ee3386a7f15d2559ab2a27a2e
Dan Hentschel <dan@hentschels.com>
2021-12-12 03:53:01 +0000

    More staging stuff

--- integration/wait_for_api_to_be_ready.ts
+++ integration/wait_for_api_to_be_ready.ts
@@ -2,16 +2,23 @@ import { API } from "aws-amplify";
 import { amplifyConfigure, amplifySignIn } from "./api_configure";
 
 const MINUTES_TO_WAIT = 5;
+const SUCCESSES_TO_WAIT_FOR = 10;
 
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
 const post = async (apiName: any, path: any, init: any): Promise<any> => {
     const start = Date.now();
     let lastError: any;
+    let successes = 0;
 
     do {
         try {
-            return await API.post(apiName, path, init);
+            const result = await API.post(apiName, path, init);
+            console.debug(`SUCCESS: ${result.message}`);
+            successes++;
+            if (successes == SUCCESSES_TO_WAIT_FOR) {
+                return;
+            }
         } catch (e) {
             console.debug(`FAILED: ${e.message}`);
             if (e.message !== 'Request failed with status code 403') {
@@ -32,6 +39,5 @@ const post = async (apiName: any, path: any, init: any): Promise<any> => {
     await amplifySignIn();
 
     console.debug('Attempting to post');
-    const result = await post('hello', '/hello', { body: { name: 'Fred' } });
-    console.debug(`SUCCESS: ${result.message}`);
+    await post('hello', '/hello', { body: { name: 'Fred' } });
 })();
