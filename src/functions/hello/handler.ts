1ba279bcc7a3d49d3f5b7a87b8797fcdefad7c68
Dan Hentschel <dan@hentschels.com>
2021-12-23 17:47:19 +0000

    Add support for Google ID

--- src/functions/hello/handler.ts
+++ src/functions/hello/handler.ts
@@ -1,12 +1,13 @@
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
 import { formatJSONResponse } from '@libs/apiGateway';
-import { getCognitoId } from '@libs/cognito';
+import { getAuthId } from '@libs/authId';
 import { middyfy } from '@libs/lambda';
 
 import schema from './schema';
 
 const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
-  const id = getCognitoId(event.requestContext.identity).userPoolUserId;
+  console.log(JSON.stringify(event.requestContext));
+  const id = getAuthId(event.requestContext.identity).authId;
   return formatJSONResponse({
     message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
     cognitoId: id,
--- src/libs/authId.spec.ts
+++ src/libs/authId.spec.ts
@@ -1,32 +1,32 @@
 import { APIGatewayEventIdentity } from 'aws-lambda';
-import { getCognitoId } from './cognito';
+import { getAuthId } from './authId';
 
 const realProviderInfo = 'cognito-idp.us-east-1.amazonaws.com/us-east-1_HqMx1CWjI,cognito-idp.us-east-1.amazonaws.com/us-east-1_HqMx1CWjI:CognitoSignIn:eb4b889f-7ad8-4025-9013-8816b6ba0c90';
 
 test('should parse pool id from real provider info', () => {
-    const { userPoolId } = getCognitoId(makeCognitoIdStructure(realProviderInfo));
+    const { userPoolId } = getAuthId(makeCognitoIdStructure(realProviderInfo));
 
     expect(userPoolId).toBe('us-east-1_HqMx1CWjI');
 });
 
 test('should parse user id from real provider info', () => {
-    const { userPoolUserId } = getCognitoId(makeCognitoIdStructure(realProviderInfo));
+    const { userPoolUserId } = getAuthId(makeCognitoIdStructure(realProviderInfo));
 
     expect(userPoolUserId).toBe('eb4b889f-7ad8-4025-9013-8816b6ba0c90');
 });
 
 test('should throw an exception for malformed provider info', () => {
-    expect(() => getCognitoId(makeCognitoIdStructure('Something crazy here'))).toThrow();
+    expect(() => getAuthId(makeCognitoIdStructure('Something crazy here'))).toThrow();
 });
 
 test('should throw if "CognitoSignIn" not present in provider info', () => {
-    expect(() => getCognitoId(makeCognitoIdStructure(
+    expect(() => getAuthId(makeCognitoIdStructure(
         'cognito-idp.us-east-1.amazonaws.com/us-east-1_HqMx1CWjI,cognito-idp.us-east-1.amazonaws.com/us-east-1_HqMx1CWjI:NotCognito:eb4b889f-7ad8-4025-9013-8816b6ba0c90'
     ))).toThrow();
 });
 
 test('should throw if userPoolUserId doesn\'t match the expected pattern', () => {
-    expect(() => getCognitoId(makeCognitoIdStructure(
+    expect(() => getAuthId(makeCognitoIdStructure(
         'cognito-idp.us-east-1.amazonaws.com/us-east-1_HqMx1CWjI,cognito-idp.us-east-1.amazonaws.com/us-east-1_HqMx1CWjI:CognitoSignIn:eb4b89f-7d8-425-913-816b6ba0c90'
     ))).toThrow();
 });
